227b6f3affb3faf88264051f93f5f54054de3845
CPADM001\CPPBHBK <bharat_kammakatla@cathaypacific.com>
2021-12-08 16:35:16 +0800

    Initial Commit

---
+++ src/com/practice/binary_search/BinarySearch.java
@@ -0,0 +1,32 @@
+package com.practice.binary_search;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] input_array = {3, 6, 8, 9, 12, 15};
+        int target = 55;
+        System.out.println(find_element(input_array, target));
+
+    }
+
+    // Find the target element in an array and return its index
+    // Return -1 if not found
+    static int find_element(int[] array, int target){
+        int start_index = 0;
+        int end_index = array.length -1;
+
+        while (start_index < end_index){
+            int middle_index = (start_index+end_index)/2;
+            if (target < array[middle_index]){
+               end_index = middle_index -1;
+            }else if (target > array[middle_index]){
+                start_index = middle_index + 1;
+            }else{
+                return middle_index;
+            }
+        }
+
+        return -1;
+    }
+
+}
