bd67ec05111b74ecd87b937d1a2ca6342d1f6c05
Bharat Kammakatla <bharatkammakatla@gmail.com>
2021-12-10 21:11:47 +0800

    Updated solution for CeilingOfANumber

--- src/com/practice/binary_search/CeilingOfANumber.java
+++ src/com/practice/binary_search/CeilingOfANumber.java
@@ -2,30 +2,27 @@ package com.practice.binary_search;
 
 public class CeilingOfANumber {
     public static void main(String[] args) {
-        int[] input_array = {1, 3, 6, 8, 9, 12, 20};
-        int target = 10;
+        int[] input_array = {3, 6, 7, 8, 9, 12, 20};
+        int target = 4;
         System.out.println(find_ceiling_number(input_array, target));
     }
-    // return the smallest number >= target
+
     static int find_ceiling_number(int[] array, int target){
-
-        // What if the target is greater than the largest number
-        if(target > array[array.length-1])
-            return array[array.length-1];
-
         int start_index = 0;
         int end_index = array.length -1;
-
         while(start_index<=end_index){
-            int mid_index = start_index + (end_index-start_index)/2;
-            if (target > array[mid_index]){
-                start_index = mid_index + 1;
-            }else if (target < array[mid_index]){
+            int mid_index = start_index + (end_index - start_index)/2;
+            if (target < array[mid_index]){
                 end_index = mid_index - 1;
-            }else{
+            }else if(target > array[mid_index]){
+                start_index = mid_index + 1;
+            }else {
                 return array[mid_index];
             }
         }
-        return array[start_index];
+        if (start_index > array.length-1)
+            return -1;
+        else
+            return array[start_index];
     }
 }
